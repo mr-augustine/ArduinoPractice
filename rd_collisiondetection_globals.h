c1f48a7d5464138e089543539dea4728d04b6c17
Augustine <pushbuttonrobotics@gmail.com>
2014-05-21 22:41:10 -0500

    First commit

---
+++ rd_collisiondetection_globals.h
@@ -0,0 +1,6 @@
+////////////////////////////////////////////////////////////////////////////////
+// Defines
+#define SPEED_CONTROLLER_DELAY_SEC         5.0
+#define LOOP_FREQUENCY_HZ                  40.0
+#define GASBRAKE_NEUTRAL                   1500
+#define STEERING_NEUTRAL                   1500
