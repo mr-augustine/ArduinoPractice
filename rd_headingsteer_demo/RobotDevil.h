7ae51622ad8f38d76fc699ed53553705d746eed6
Augustine <pushbuttonrobotics@gmail.com>
2014-06-03 22:50:27 -0500

    First commit

---
+++ rd_headingsteer_demo/RobotDevil.h
@@ -0,0 +1,12 @@
+#ifndef _RobotDevil_h_
+#define _RobotDevil_h_
+
+void init_pushbutton(void);
+void update_pushbutton(void);
+
+void init_compass(void);
+void update_compass(void);
+
+void update_control_values(void);
+
+#endif
