248f46848c61b58bfec5962a3f0b7d7e4e4c5f8d
Augustine <pushbuttonrobotics@gmail.com>
2014-06-05 23:03:03 -0500

    Added control code and constants
    This code hasn't been tested yet

--- rd_headingsteer_demo/mobility.h
+++ rd_headingsteer_demo/mobility.h
@@ -14,8 +14,11 @@
 
 #define STEERING_NEUTRAL             1500
 #define GASBRAKE_NEUTRAL             1500
-
-
+#define STEERING_MIN                 1100
+#define STEERING_MAX                 1900
+#define GASBRAKE_MIN                 1100
+#define GASBRAKE_MAX                 1900
+#define GASBRAKE_CRUISE              1700
 
 ////////////////////////////////////////////////////////////////////////////////
 // Interrupt Service Routines
@@ -31,10 +34,32 @@ ISR(TIMER1_COMPB_vect) {
 
 ////////////////////////////////////////////////////////////////////////////////
 // Mobility Variables
-
+float previous_hdgerror_deg = 0.0;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Mobility Functions
+void update_heading_error(void) {
+  //globals.heading_error_deg = 0.0;
+  float heading_delta = 0.0;
+  
+  if (globals.status_bits & STATUS_COMPASS_VALID == STATUS_COMPASS_VALID) {
+    heading_delta = globals.target_heading_deg - globals.compass_deg;
+   
+    if (heading_delta > 180.0) {
+      heading_delta -= 360.0;
+    }
+    else if (heading_delta < -180.0) {
+      heading_delta += 360.0;
+    }
+    
+    previous_hdgerror_deg = globals.heading_error_deg;
+    globals.heading_error_deg = heading_delta;
+    // negative heading_error values indicate that you need to steer left to
+    // move more directly towards the target, and vice versa
+  }
+}
+
+
 void update_control_values(void) {
   float seconds = globals.loop_counter / LOOP_FREQUENCY_HZ;
   
@@ -52,10 +77,43 @@ void update_control_values(void) {
     return;
   }
   
-  /*
+  // hdgerror_rate_dps is the heading error rate in degrees per second;
+  // i.e., the rate you are closing in or drifting away from the target heading.
+  // if the vehicle is closing in towards the target heading, this value will
+  // be positive.
+  float hdgerror_rate_dps = 
+      (previous_hdgerror_deg - globals.heading_error_deg) / 
+      (1 / LOOP_FREQUENCY_HZ);
+    
+  float steering_k_diff = PID_K_DIFF * hdgerror_rate_dps;  
+  float steering_k_prop = PID_K_PROP * globals.heading_error_deg;
   
-  INSERT CONTROL CODE HERE
-  */  
+  // the corrected steering control includes the proportional and differential
+  // components. the proportional component should always direct the vehicle
+  // toward the target heading, and the differential component should
+  // simultaneously direct the vehicle away from the target heading.
+  float steering = globals.steering_servo_us + steering_k_prop;
+  if (globals.heading_error_deg <= 0 ) {
+    // if the vehicle needs to turn left towards the target, add right-turn
+    // differential correction. remember: smaller PWM values steer left, and
+    // larger PWM values steer right.
+    steering += steering_k_diff;
+  }
+  // otherwise, if the vehicle needs to turn right towards the target, add
+  // left-turn differential correction
+  else {
+    steering -= steering_k_diff;
+  }
+  
+  if (steering < STEERING_MIN) {
+    steering = STEERING_MIN;
+  }
+  else if (steering > STEERING_MAX) {
+    steering = STEERING_MAX;
+  }
+  
+  globals.steering_servo_us = (uint16_t) steering;
+  globals.gasbrake_servo_us = GASBRAKE_CRUISE;
 
   return;
 }
