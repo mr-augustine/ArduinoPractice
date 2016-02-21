9776cdda57494a39d1742c35f2eeb25ff7506757
mr-augustine <pushbuttonrobotics@gmail.com>
2016-02-20 23:17:00 -0600

    Added eeprom writing functions

--- eeprom_lib_test/onboard_logger.h
+++ eeprom_lib_test/onboard_logger.h
@@ -7,13 +7,16 @@
 #define EEPROM_CAPACITY             1024
 #define CHUNK_SIZE                  512
 
-#define FRAME_PREFIX                0xDADAFEED;
-#define FRAME_SUFFIX                0xCAFEBABE;
-#define TOTAL_PREFIX_SUFFIX_SIZE    8
+#define FRAME_PREFIX                0xDADAFEED
+#define FRAME_PREFIX_SIZE           4
+#define FRAME_SUFFIX                0xCAFEBABE
+#define FRAME_SUFFIX_SIZE           4
+#define TOTAL_PREFIX_SUFFIX_SIZE    (FRAME_PREFIX_SIZE + FRAME_SUFFIX_SIZE)
 
 static uint8_t onboard_logger_enabled;
 
 void init_onboard_logger(const void * data, int size);
+void write_next_frame();
 //void write_next_frame(int frame_index);
 
 //void write_frame(const void * data, int size, int frame_index);
