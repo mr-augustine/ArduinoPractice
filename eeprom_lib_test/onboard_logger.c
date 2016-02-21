879b054a60028730691b22f87f6b064d74e97449
mr-augustine <pushbuttonrobotics@gmail.com>
2016-02-20 18:22:21 -0600

    Added success-checking

--- eeprom_lib_test/onboard_logger.c
+++ eeprom_lib_test/onboard_logger.c
@@ -12,26 +12,25 @@ int num_chunks;
 int num_frames;
 
 //uint8_t onboard_logger_enabled;
-static void set_capacity();
-static void set_chunk_index();
-static void set_data_source(const void * data_ptr);
-static void set_num_chunks();
-static void set_num_frames(int frame_size);
+static uint8_t set_capacity();
+static uint8_t set_chunk_index();
+static uint8_t set_data_source(const void * data_ptr);
+static uint8_t set_num_chunks();
+static uint8_t set_num_frames(int frame_size);
 
-// TODO consider changing the static functions so that they return
-// 1 on success and 0 otherwise; use an if-conditional chain to
-// evaluate the success of all static function calls; set the
-// onboard_logging_enabled variable accordingly
-// if (func1 && func2 ... && funcN) {onboard_logging enabled = 1;}
 void init_onboard_logger(const void * data, int size) {
-    set_capacity();
-    set_num_chunks();
-    set_num_frames(size);
-    set_data_source(data);
-    set_chunk_index();
+    onboard_logger_enabled = 0;
+
+    if (set_capacity() &&
+        set_num_chunks() &&
+        set_num_frames(size) &&
+        set_data_source(data) &&
+        set_chunk_index()) {
+
+        onboard_logger_enabled = 1;
+    }
 
     frame_index = 0;
-    onboard_logger_enabled = 1;
 
     return;
 }
@@ -49,57 +48,73 @@ uint8_t onboard_logger_enabled() {
 }*/
 
 /* Validates and sets the total EEPROM capacity */
-static void set_capacity() {
+static uint8_t set_capacity() {
+    uint8_t success = 0;
     eeprom_capacity = 0;
 
     // Supporting only Uno and Mega
     if (EEPROM_CAPACITY == 1024 ||
         EEPROM_CAPACITY == 4096) {
         eeprom_capacity = EEPROM_CAPACITY;
+
+        success = 1;
     } 
 
-    return;
+    return success;
 }
 
 // TODO consider using rand() to choose a chunk index; decide what seed
 // value to use
-static void set_chunk_index() {
+static uint8_t set_chunk_index() {
+    uint8_t success = 1;
     chunk_index = 0;
 
     if (num_chunks > 1) {
         //choose a random chunk
+    } else if (num_chunks <= 0) {
+        success = 0;
     }
 
-    return;
+    return success;
 }
 
-static void set_data_source(const void * data_ptr) {
+static uint8_t set_data_source(const void * data_ptr) {
+    uint8_t success = 0;
+
     if (data_ptr != NULL) {
         data_source = data_ptr;
+
+        success = 1;
     }
 
-    return;
+    return success;
 }
 
 /* Calculates the whole number of chunks that can be written to */
-static void set_num_chunks() {
+static uint8_t set_num_chunks() {
+    uint8_t success = 0;
     num_chunks = 0;
 
     if (CHUNK_SIZE > TOTAL_PREFIX_SUFFIX_SIZE) {
         num_chunks = eeprom_capacity / CHUNK_SIZE;
+
+        success = 1;
     }
 
-    return;
+    return success;
 }
 
-static void set_num_frames(int frame_size) {
+static uint8_t set_num_frames(int frame_size) {
+    uint8_t success = 0;
     num_frames = 0; 
 
     if (frame_size > 0) {
         num_frames = CHUNK_SIZE / frame_size;
+
+        success = 1;
     }
 
-    return;
+    return success;
 }
 
 
