055e62f0f237393f92c44fb637774e787a1e007e
mr-augustine <pushbuttonrobotics@gmail.com>
2016-03-24 20:03:12 -0500

    Updated uwrite library

--- sd_card_logger/uwrite.h
+++ sd_card_logger/uwrite.h
@@ -1,23 +1,12 @@
-/*
- *
- */
-#ifndef _uwrite_H_
-#define _uwrite_H_
-
-#define RCIE0   7
-#define RXEN0   4
-#define TXEN0   3
-#define UCSZ01  2
-#define UCSZ00  1
-#define UDRE0   5
+#ifndef _UWRITE_H_
+#define _UWRITE_H_
 
 #define BUFF_SIZE 16
 
 void uwrite_init(void);
-void uwrite_print_buff(const char * char_buff);
-void uwrite_print_byte(const void * a_byte);
-void uwrite_print_short(const void * a_short);
-void uwrite_print_long(const void * a_long);
+void uwrite_print_buff(char * char_buff);
+void uwrite_print_byte(void * a_byte);
+void uwrite_print_short(void * a_short);
+void uwrite_print_long(void * a_long);
 
 #endif
-
