361d80bf6812fc1a746292f2131e0b3651911904
mr-augustine <pushbuttonrobotics@gmail.com>
2016-05-04 20:50:56 -0500

    Integrated sdcard; enabled return value for sdcard_init; bundled spi_init into sdcard_init

--- data_demo/main.c
+++ data_demo/main.c
@@ -12,6 +12,7 @@
 #include "pins.h"
 #include "cmps10.h"
 #include "ledbutton.h"
+#include "sd_card.h"
 #include "statevars.h"
 #include "uwrite.h"
 
@@ -20,30 +21,49 @@ statevars_t statevars;
 int main(void) {
   char msg[64];
 
+  //TODO: Verify that all init functions were successful before proceeding
   button_init();
   uwrite_init();
   cmps10_init();
+  sdcard_init();
 
   memset(msg, 0, sizeof(msg));
+  memset(&statevars, 0, sizeof(statevars));
+  statevars.prefix = 0xDADAFEED;
+  statevars.suffix = 0xCAFEBABE;
 
-  while (1) {
+  //uint32_t iterations = 0;
+
+  /*while (1) {
     button_update();
     cmps10_update_all();
+    statevars.main_loop_counter = iterations;
 
     if (button_is_pressed()) {
       uwrite_print_buff("The button is pressed! The LED should be on.\r\n");
       led_turn_on();
+      statevars.mission_started = 1;
     } else {
       led_turn_off();
+      statevars.mission_started = 0;
     }
 
     snprintf(msg, sizeof(msg),
       "Heading: %u  Pitch: %d  Roll: %hhd\r\n",
-     //cmps10_heading, cmps10_pitch, cmps10_roll); 
+     cmps10_heading, cmps10_pitch, cmps10_roll); 
      // Needed to cast these values to display negative pitch and roll values
-     (int8_t) statevars.heading_raw, (int8_t) statevars.pitch_deg, (int8_t) statevars.roll_deg); 
+     //(int8_t) statevars.heading_raw, (int8_t) statevars.pitch_deg, (int8_t) statevars.roll_deg); 
     uwrite_print_buff(msg);
-  }
+
+    //sdcard_write_data();
+
+    iterations++;
+
+    if (iterations > 2048) {
+      uwrite_print_buff("Finished collecting data!\r\n");
+      break;
+    }
+  }*/
 
   return 0;
 }
